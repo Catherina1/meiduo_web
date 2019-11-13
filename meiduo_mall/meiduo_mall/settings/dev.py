c0b4c984440f1d9c4f1a68c2e3b2aa1929d5c5a6
chenqx <723102747@qq.com>
2019-11-13 19:33:18 +0800

    创建新的应用，追加导包路径

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -10,11 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-import os
+import os,sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(BASE_DIR)
+
+# 追加导包路径
+# print(sys.path)  #'E:\\PythonProject\\project\\meiduo_web'
+# print(BASE_DIR) #E:\PythonProject\project\meiduo_web\meiduo_mall\meiduo_mall
+sys.path.insert(0,os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'meiduo_mall.app.users',
+    'users',  # 追加导包路径后添加app时简单很多
 ]
 
 MIDDLEWARE = [
@@ -189,3 +195,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
