756e877a52a45ea91411883573c95f9577da2a35
chenqx <723102747@qq.com>
2019-11-26 17:54:32 +0800

    后端视图函数校验修改，判断少了not，低级错误

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,6 +1,8 @@
 from django import http
-from django.shortcuts import render,HttpResponse
+from django.db import DatabaseError
+from django.shortcuts import render
 from django.views import View
+from users.models import User
 import re
 # Create your views here.
 
@@ -30,20 +32,26 @@ class Register(View):
         if not all([username,password,password2,mobile,allow]): # 这个all()参数要求可迭代对象，有False就False
             return http.HttpResponseForbidden('缺少必要的参数哦')
         # 判断用户名是否是5-20个字符
-        if re.match(r'^[a-zA-Z0-9_-]{5,20}$',username):
+        if not re.match(r'^[a-zA-Z0-9_]{5,20}$',username):
             return http.HttpResponseForbidden('用户格式错误')
         # 判断密码是否是8-20个字
-        if re.match(r'^[0-9a-zA-Z]{8,20}$',password):
-            return http.HttpResponseForbidden('用户格式错误')
+        if not re.match(r'^[0-9a-zA-Z]{8,20}$',password):
+            return http.HttpResponseForbidden('密码格式错误')
         # 判断两次密码是否一致
-        if password2 != password:
+        if not password2 != password:
             return http.HttpResponseForbidden('两次密码输入不对')
         # 判断手机号是否合法
-        if re.match(r'^1[3-9]\d{9}$',mobile):
+        if not re.match(r'^1[3-9]\d{9}$',mobile):
             return http.HttpResponseForbidden('手机号不合法')
         # 判断是否勾选用户协议
         if allow != 'on':
             return http.HttpResponseForbidden("请同意协议")
+
+        try:
+            User.objects.create_user(username=username,password=password,mobile=mobile,allow=allow)
+        except DatabaseError:
+            return render('register.html', {'register_errmsg': '注册失败'})
+
 
 
 
