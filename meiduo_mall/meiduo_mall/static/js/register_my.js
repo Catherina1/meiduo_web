9792f9e07406ce1134bf6a61e12a58ce8ac317dc
chenqx <723102747@qq.com>
2019-11-26 11:33:44 +0800

    解决加载无显示问题（不是v-cloak）是<script>放在头还是尾的问题

--- meiduo_mall/meiduo_mall/static/js/register_my.js
+++ meiduo_mall/meiduo_mall/static/js/register_my.js
@@ -22,23 +22,58 @@ let vm = new Vue({
 
     },
     method:{
+        //blur光标触发的时候，会执行以下方法
         check_username(){
-
+            let re = /^[a-zA-Z0-9_-]{5,20}$/;
+            if(re.test(this.username)){
+                this.error_username = false;
+            }else{
+                this.error_username = true;
+            }
         },
         check_password(){
-
+            let re = /^[0-9A-Za-z]{8,20}$/;
+            if(re.test(this.password)){
+                this.error_password = false;
+            }else{
+                this.error_password = true;
+            }
         },
         check_password2(){
-
+            if(this.password2 != this.password){
+                this.error_password2 = true;
+            }else{
+                this.error_password2 = false;
+            }
         },
         check_mobile(){
-
+            let re = /^1[3-9]\d{9}$/;
+            if(re.test(this.mobile)){
+                this.error_mobile = false;
+            }else {
+                this.error_mobile = true;
+            }
         },
-        check_sms_code(){
-
+        check_allow(){
+          if(!this.allow){
+              this.error_allow = false;
+          }else {
+              this.error_allow = true;
+          }
         },
+        // check_sms_code(){
+        //
+        // },
         on_submit(){
-
+            check_username();
+            check_password();
+            check_password2();
+            check_mobile();
+            check_allow();
+            //这块是为了检测有哪些报错的地方，如有不合适的地方则需要停止表单的提交
+            if(this.error_username==true | this.error_mobile==true | this.error_password==true | this.error_password2==true | this.error_allow==true){
+                window.event.returnValue = false;
+            }
         },
     }
 });
--- meiduo_mall/meiduo_mall/utils/jinja2_env.py
+++ meiduo_mall/meiduo_mall/utils/jinja2_env.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 def jinja2_environment(**options):
     env = Environment(**options)
     env.globals.update({
-        'static': staticfiles_storage,  # 为了方便后面使用这个原生的模板方法{{ static（‘’） }}
+        'static': staticfiles_storage.url,  # 为了方便后面使用这个原生的模板方法{{ static（‘’） }},还有别忘记.url
         'url': reverse,  # 同理啊，一样是加了一个{{ url('') }}
     })
     return env
