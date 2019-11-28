6679f7daf40a94e7ba913e16250d4d2af5cc3bab
chenqx <723102747@qq.com>
2019-11-28 19:52:54 +0800

    在users的view下面进行短信验证码的验证，Post验证的再使用jinja模板变量返回报错

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -4,6 +4,7 @@ from django.db import DatabaseError
 from django.shortcuts import render,redirect
 from django.urls import reverse
 from django.views import View
+from django_redis import get_redis_connection
 from users.models import User
 import re
 from meiduo_mall.utils.response_code import RETCODE
@@ -21,7 +22,6 @@ from meiduo_mall.utils.response_code import RETCODE
 # 这里使用了类视图写法,一些处理都被类视图封装起来了
 
 
-
 class Register(View):
     def get(self, request):
         return render(request, 'register.html')
@@ -33,9 +33,10 @@ class Register(View):
         password2 = request.POST.get('password2')
         mobile = request.POST.get('mobile')
         allow = request.POST.get('allow')
+        sms_code_client = request.POST.get('sms_code')
 
         # 判断参数是否齐全
-        if not all([username,password,password2,mobile,allow]): # 这个all()参数要求可迭代对象，有False就False
+        if not all([username,password,password2,mobile,sms_code_client,allow]): # 这个all()参数要求可迭代对象，有False就False
             return http.HttpResponseForbidden('缺少必要的参数哦')
         # 判断用户名是否是5-20个字符
         if not re.match(r'^[a-zA-Z0-9_]{5,20}$',username):
@@ -49,6 +50,15 @@ class Register(View):
         # 判断手机号是否合法
         if not re.match(r'^1[3-9]\d{9}$',mobile):
             return http.HttpResponseForbidden('手机号不合法')
+
+        # 验证短信验证码
+        redis_cnn = get_redis_connection('verify_code')
+        sms_code_server = redis_cnn.get('sms_%s' % mobile)
+        if sms_code_server is None:
+            return render(request, 'register.html', {'sms_code_errmsg': '无效的短信验证码'})
+        if sms_code_client != sms_code_server.decode():
+            return render(request, 'register.html', {'sms_code_errmsg': '短信验证码有误'})
+
         # 判断是否勾选用户协议
         if allow != 'on':
             return http.HttpResponseForbidden("请同意协议")
