aa1943915c417c208d02a17cfb53ef27f6370d40
chenqx <723102747@qq.com>
2020-01-08 14:59:25 +0800

    封装获取商品分类的方法

---
+++ meiduo_mall/meiduo_mall/apps/contents/utils.py
@@ -0,0 +1,34 @@
+from collections import OrderedDict
+from .models import  ContentCategory
+from goods.models import GoodsCategory,GoodsChannel,GoodsChannelGroup
+
+
+def get_categories(self, request):
+    """获取商品的分类"""
+    # 1.展示商品分类
+    # categories = {}  # 整个大字典需要传给前端的数据
+    categories = OrderedDict()  # 商品分类对应的字典（s使用这种字典可以是有序的字典）
+    # 37个一级类别（所有商品频道）len(channel)=37
+    channels = GoodsChannel.objects.all()
+    # 将大致框架搭建好，遍历所有频道
+    for channel in channels:
+        # 获取当前频道所在的组,len(group_id)=11
+        group_id = channel.group_id
+        # 构造基本一级数据框架：只有11个组
+        if group_id not in categories:
+            categories[group_id] = {"channels": [], "sub_cats": []}  # 一级
+        # 查询当前拼频道对应的一级分类
+        cat1 = channel.category  # channels 分类，channels外键
+        # channels 列表赋值
+        categories[group_id]['channels'].append({
+            'id': cat1.id,
+            'name': cat1.name,  # cat1 = 手机
+            'url': channel.url
+        })
+        # 二级（cat1 的所有下级数据（Goodscategory是个自关联的表））
+        for cat2 in cat1.subs.all():
+            cat2.sub_cats = []
+            for cat3 in cat2.subs.all():
+                cat2.sub_cats.append(cat3)
+            categories[group_id]['sub_cats'].append(cat2)
+    return categories
--- meiduo_mall/meiduo_mall/apps/contents/views.py
+++ meiduo_mall/meiduo_mall/apps/contents/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from goods.models import GoodsCategory,GoodsChannel,GoodsChannelGroup
-from .models import ContentCategory,Content
+from .models import ContentCategory
+from .utils import get_categories
 
 
 # Create your views here.
@@ -9,28 +10,7 @@ class IndexView(View):
     def get(self,request):
         # 展示首页
         # 1.展示商品分类
-        categories = {}  # 整个大字典需要传给前端的数据
-        channels = GoodsChannel.objects.all()  # 37个分类,group一共7个组 len(channel)=37
-        # 将大致框架搭建好
-        for channel in channels:
-            group_id = channel.group_id  # len(group_id)=7
-            # 按照小组分组，7个小组
-            if group_id not in categories:
-                categories[group_id] = {"channels": [], "sub_cats": []}  # 一级
-            cat1 = channel.category  # channels 分类，channels外键
-            # channels 列表赋值
-            categories[group_id]['channels'].append({
-                'id': cat1.id,
-                'name': cat1.name,  # cat1 = 手机
-                'url': channel.url
-            })
-            # 二级（cat1 的所有下级数据（Goodscategory是个自关联的表））
-            for cat2 in cat1.subs.all():
-                cat2.sub_cats = []
-                for cat3 in cat2.subs.all():
-                    cat2.sub_cats.append(cat3)
-                categories[group_id]['sub_cats'].append(cat2)
-
+        categories = get_categories(self, request)
         # 2.展示轮播图等图片商品广告
         content = {}
         # 获取广告分类（分类一共有25种）
