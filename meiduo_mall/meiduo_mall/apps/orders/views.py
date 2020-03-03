eadb1ff192fb7a2e505853f1a70194d70b18a52d
chenqx <723102747@qq.com>
2020-03-03 18:41:13 +0800

    使用事务保存订单数据

--- meiduo_mall/meiduo_mall/apps/orders/views.py
+++ meiduo_mall/meiduo_mall/apps/orders/views.py
@@ -2,6 +2,7 @@ import json
 from decimal import Decimal
 from django import http
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db import transaction
 from django.shortcuts import render
 # Create your views here.
 from django.utils import timezone
@@ -83,75 +84,85 @@ class OrderCommitView(LoginRequiredJSONMixin, View):
             return http.HttpResponseForbidden('缺少必传参数')
         # 判断address_id是否合法
         try:
-            address = Address.objects.get(id=address_id)
+            address = Address.objects.get(id=address_id)  # 不用filter是因为这个会有缓存
         except Exception:
             return http.HttpResponseForbidden('参数address_id错误')
         # 判断pay_method是否合法
         if pay_method not in [OrderInfo.PAY_METHODS_ENUM['CASH'], OrderInfo.PAY_METHODS_ENUM['ALIPAY']]:
             return http.HttpResponseForbidden('参数pay_method错误')
 
-        # 3.获取登录用户
-        user = request.user
-        # 4.生成订单编号：年月日时分秒+用户编号
-        order_id = timezone.localtime().strftime('%Y%m%d%H%M%S') + ('%09d' % user.id)
-        # 5.保存订单基本信息 OrderInfo（一）
-        order = OrderInfo.objects.create(
-            order_id=order_id,
-            user=user,
-            address=address,
-            total_count=0,
-            total_amount=Decimal('0'),
-            freight=Decimal('10.00'),
-            pay_method=pay_method,
-            status=OrderInfo.ORDER_STATUS_ENUM['UNPAID'] if pay_method == OrderInfo.PAY_METHODS_ENUM['ALIPAY'] else
-            OrderInfo.ORDER_STATUS_ENUM['UNSEND']
-        )
+        with transaction.atomic():
+            # 创建事务保存点
+            save_id = transaction.savepoint()
+            try:
+                # 3.获取登录用户
+                user = request.user
+                # 4.生成订单编号：年月日时分秒+用户编号
+                order_id = timezone.localtime().strftime('%Y%m%d%H%M%S') + ('%09d' % user.id)
+                # 5.保存订单基本信息 OrderInfo（一）
+                order = OrderInfo.objects.create(
+                    order_id=order_id,
+                    user=user,
+                    address=address,
+                    total_count=0,
+                    total_amount=Decimal('0'),
+                    freight=Decimal('10.00'),
+                    pay_method=pay_method,
+                    status=OrderInfo.ORDER_STATUS_ENUM['UNPAID'] if pay_method == OrderInfo.PAY_METHODS_ENUM['ALIPAY'] else
+                    OrderInfo.ORDER_STATUS_ENUM['UNSEND']
+                )
 
-        # 从redis读取购物车中被勾选的商品信息
-        redis_conn = get_redis_connection('carts')
-        redis_cart = redis_conn.hgetall('carts_%s' % user.id)
-        selected = redis_conn.smembers('selected_%s' % user.id)
-        carts = {}
-        for sku_id in selected:
-            carts[int(sku_id)] = int(redis_cart[sku_id])
-        sku_ids = carts.keys()
+                # 从redis读取购物车中被勾选的商品信息
+                redis_conn = get_redis_connection('carts')
+                redis_cart = redis_conn.hgetall('carts_%s' % user.id)
+                selected = redis_conn.smembers('selected_%s' % user.id)
+                carts = {}
+                for sku_id in selected:
+                    carts[int(sku_id)] = int(redis_cart[sku_id])
+                sku_ids = carts.keys()
 
-        # 遍历购物车中被勾选的商品信息
-        for sku_id in sku_ids:
-            # 查询SKU信息
-            sku = SKU.objects.get(id=sku_id)
-            # 判断SKU库存
-            sku_count = carts[sku.id]
-            if sku_count > sku.stock:
-                return http.JsonResponse({'code': RETCODE.STOCKERR, 'errmsg': '库存不足'})
+                # 遍历购物车中被勾选的商品信息
+                for sku_id in sku_ids:
+                    # 查询SKU信息
+                    sku = SKU.objects.get(id=sku_id)
+                    # 判断SKU库存
+                    sku_count = carts[sku.id]
+                    if sku_count > sku.stock:
+                        # 出错就回滚
+                        transaction.savepoint_rollback(save_id)
+                        return http.JsonResponse({'code': RETCODE.STOCKERR, 'errmsg': '库存不足'})
 
-            # sku减少库存
-            sku.stock -= sku_count
-            # sku增加销售量
-            sku.sales += sku_count
-            sku.save()
+                    # sku减少库存
+                    sku.stock -= sku_count
+                    # sku增加销售量
+                    sku.sales += sku_count
+                    sku.save()
 
-            # spu增加
-            # (当某一型号手机卖出，那么这一大类数量也会增加)
-            sku.spu.sales += sku_count
-            sku.spu.save()
+                    # spu增加
+                    # (当某一型号手机卖出，那么这一大类数量也会增加)
+                    sku.spu.sales += sku_count
+                    sku.spu.save()
 
-            # 保存订单商品信息 OrderGoods（多）
-            OrderGoods.objects.create(
-                order=order,
-                sku=sku,
-                count=sku_count,
-                price=sku.price,
-            )
+                    # 保存订单商品信息 OrderGoods（多）
+                    OrderGoods.objects.create(
+                        order=order,
+                        sku=sku,
+                        count=sku_count,
+                        price=sku.price,
+                    )
 
-            # 保存商品订单总价和数量
-            order.total_count += sku_count
-            order.total_amount += (sku_count * sku.price)
+                    # 保存商品订单总价和数量
+                    order.total_count += sku_count
+                    order.total_amount += (sku_count * sku.price)
 
-        # 添加邮费和保存订单信息
-        order.total_amount += order.freight
-        order.save()
-
+                # 添加邮费和保存订单信息
+                order.total_amount += order.freight
+                order.save()
+            except Exception as e:
+                transaction.savepoint_rollback(save_id)
+                return http.JsonResponse({'code': RETCODE.DBERR, 'errmsg': '下单失败'})
+            # 提交订单成功，显式的提交一次事务
+            transaction.savepoint_commit(save_id)
         # 响应提交订单结果
         return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '下单成功', 'order_id': order.order_id})
 
