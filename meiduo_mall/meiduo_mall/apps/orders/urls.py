69461218d6c76787f58f06937e80c0ece3054cbe
chenqx <723102747@qq.com>
2020-03-11 13:34:51 +0800

    商品评价功能实现

--- meiduo_mall/meiduo_mall/apps/orders/urls.py
+++ meiduo_mall/meiduo_mall/apps/orders/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^orders/settlement/$', views.OrderSettlementView.as_view(), name='order'),
     url(r'^orders/commit/$', views.OrderCommitView.as_view(), name='order_commit'),
     url(r'^orders/success/$', views.OrderSuccessView.as_view(), name='order_success'),
-    url(r'^orders/info/(?P<page_num>\d+)/$', views.UserOrderInfoView.as_view(), name='info')
+    url(r'^orders/info/(?P<page_num>\d+)/$', views.UserOrderInfoView.as_view(), name='info'),
+    url(r'^orders/comment/$', views.OrderCommentView.as_view(), name='comment'),
 ]
